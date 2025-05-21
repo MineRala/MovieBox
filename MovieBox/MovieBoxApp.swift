2eb719f2096e052aab85d90c79800f72c6367213
MineRala <mine-rala@hotmail.com>
2025-05-21 10:57:39 +0300

    Initial Commit

---
+++ MovieBox/MovieBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieBoxApp.swift
+//  MovieBox
+//
+//  Created by Mine Rala on 21.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct MovieBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
